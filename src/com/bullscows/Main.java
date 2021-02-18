6e59c7d161b1f9b481704d645ef9db0d102b5200
NikitaMesn <nikita.mesn@gmail.com>
2021-02-18 15:48:52 +0200

    hard version

--- src/com/bullscows/Main.java
+++ src/com/bullscows/Main.java
@@ -4,12 +4,13 @@ import java.util.*;
 
 
 public class Main {
-    public static int attemptCounter = 0;
-    public static String randomCode = "";
-    public static int numDigCod = 0;
-    public static boolean endGame = false;
-    public static int bulls = 0;
-    public static int cows = 0;
+    private static int attemptCounter = 0;
+    private static String randomCode = "";
+    private static int lenCode = 0;
+    private static int numSymbols = 0;
+    private static boolean endGame = false;
+    private static int bulls = 0;
+    private static int cows = 0;
 
 
     public static void main(String[] args) {
@@ -17,28 +18,28 @@ public class Main {
         guessCode();
 
     }
-    public static void startGame() {
+    private static void startGame() {
         Scanner sc = new Scanner(System.in);
-        System.out.println("Please, enter the secret code's length:");
-        numDigCod = sc.nextInt();
-        if (numDigCod > 10) {
-            System.out.printf("Error: can't generate a secret number " +
-                            "with a length of %s because there aren't enough unique digits. \n",
-                    numDigCod);
-            numDigCod = 0;
+        System.out.println("Input the length of the secret code:");
+        lenCode = sc.nextInt();
+        System.out.println("Input the number of possible symbols in the code:");
+        numSymbols = sc.nextInt();
+        if (lenCode > numSymbols || numSymbols > 36 ||  numSymbols <= 0) {
+            System.out.print("Error: can't generate a secret number \n");
+            lenCode = 0;
             startGame();
-        }else if ( numDigCod <= 0) {
-            numDigCod = 0;
+        }else if ( lenCode <= 0) {
+            lenCode = 0;
             startGame();
         } else {
-            generateCode(numDigCod);
+            generateCode();
             System.out.print("Okay, let's start a game!\n") ;
-            //System.out.println(randomCode);
+            System.out.println(randomCode);
         }
     }
 
 
-    public static void guessCode() {
+    private static void guessCode() {
         Scanner sc = new Scanner(System.in);
         attemptCounter++;
 
@@ -51,7 +52,7 @@ public class Main {
 
     }
 
-    public static void BullCowsCounter(String answer) {
+    private static void BullCowsCounter(String answer) {
         bulls = 0;
         cows = 0;
         for (int i = 0; i < answer.length(); i++) {
@@ -65,7 +66,7 @@ public class Main {
         }
     }
 
-    public static void checkAnswer (String answer) {
+    private static void checkAnswer (String answer) {
         attemptCounter++;
         BullCowsCounter(answer);
 
@@ -83,7 +84,7 @@ public class Main {
                 System.out.printf("Grade: %s bull(s).\n",
                         bulls);
             }
-        } else if (cows != 0 && bulls == 0) {
+        } else if (cows != 0) {
             System.out.printf("Grade: %s cow(s).\n",
                     cows);
         } else {
@@ -93,19 +94,45 @@ public class Main {
     }
 
 
-    public static void generateCode(int num) {
-        StringBuilder code = new StringBuilder("");
-        List<Integer> randomList = new ArrayList<>(List.of(0, 1, 2, 3, 4, 5, 6, 7, 8, 9));
+    private static void generateCode( ) {
+        StringBuilder code = new StringBuilder();
+        List<Character> chars = new ArrayList<>(List.of('0', '1', '2', '3', '4', '5', '6', '7', '8', '9',
+                'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r',
+                's', 't', 'u', 'v', 'w', 'x', 'y', 'z'));
+        String asterisk = multyString(lenCode);
+        String postAsterisk = pstAsterisk(numSymbols, chars);
+
+        List<Character> randomList = chars.subList(0, numSymbols);
         Collections.shuffle(randomList);
 
-        for (int i = 0; i < num; i++) {
-            if (i == 0 && randomList.get(i) == 0) {
-                Collections.shuffle(randomList);
-                i--;
-            }
+
+
+        for (int i = 0; i < lenCode; i++) {
             code.append(randomList.get(i));
         }
         randomCode = code.toString();
+
+        System.out.print("The secret code is prepared: " + asterisk + postAsterisk + "\n");
     }
 
+    private static String multyString( int x) {
+        String str = "";
+        for (int i = 0; i < x; i++) {
+            str += "*";
+        }
+        return str + " ";
+    }
+
+    private  static String pstAsterisk(int x, List<Character> lst) {
+        String str;
+
+        if (x <= 10) {
+            str = "(0-" + lst.get(x - 1) + ")";
+            //System.out.println(str);
+        } else {
+            str = "(0-9, a-" + lst.get(x - 1) + ")";
+        }
+        return  str;
+
+    }
 }
